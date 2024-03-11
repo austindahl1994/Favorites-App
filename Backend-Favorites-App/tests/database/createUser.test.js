6af9e41c05c05152c28c4b5467b0a9ec67162207
Austin Dahl <flyboy288@gmail.com>
2024-03-11 00:31:25 -0500

    added coverage to ignore and another db test

--- Backend-Favorites-App/tests/database/createUser.test.js
+++ Backend-Favorites-App/tests/database/createUser.test.js
@@ -1,8 +1,14 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { User } from "../../app/models/userModel.js";
-
+import { createUser } from "../../app/services/userService.js";
 let mongoServer;
+
+const userData = {
+  username: "johnnyboy",
+  email: "jb@gmail.com",
+  password: "noneyabusiness",
+};
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
@@ -18,11 +24,6 @@ afterAll(async () => {
 
 describe("User Model Test", () => {
   test("create and save user successfully", async () => {
-    const userData = {
-      username: "johnnyboy",
-      email: "jb@gmail.com",
-      password: "noneyabusiness",
-    };
     const createdUser = new User(userData);
     const savedUser = await createdUser.save();
 
@@ -31,5 +32,26 @@ describe("User Model Test", () => {
     expect(savedUser.email).toBe(userData.email);
     expect(savedUser.password).toBe(userData.password);
     expect(savedUser.createdAt).toBeDefined();
+
+    const deletedUser = await User.findOneAndDelete({ username: "johnnyboy" });
+
+    const userAfterDeletion = await User.findOne({ username: "johnnyboy" });
+    expect(userAfterDeletion).toBeNull();
+  });
+});
+
+describe("User Creation Service Test", () => {
+  test("Use createUser function properly", async () => {
+    await createUser(userData);
+
+    const createdUser = await User.findOne({ username: "johnnyboy" });
+    expect(createdUser).not.toBeNull();
+    expect(createdUser.username).toBe(userData.username);
+    expect(createdUser.email).toBe(userData.email);
+
+    const deletedUser = await User.findOneAndDelete({ username: "johnnyboy" });
+
+    const userAfterDeletion = await User.findOne({ username: "johnnyboy" });
+    expect(userAfterDeletion).toBeNull();
   });
 });
